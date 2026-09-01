e8f74853d6afd293266259d5a869e5ee949d6d88
Timur Alimov <mygamer1805@gmail.com>
2026-09-01 21:53:27 +0200

    fixed alignments for heap

--- src/react-pages/heap.jsx
+++ src/react-pages/heap.jsx
@@ -7,31 +7,34 @@ import { HashRouter as Router, Routes, Route, useNavigate } from 'react-router-d
 import { cleanup } from '@testing-library/react';
 
 
-export function Heap({heapArray, width}){
-    const MAX_AMOUNT_OF_NODES = 8;
-    const DIAMETER_OF_NODE = Math.round(width/MAX_AMOUNT_OF_NODES);
-    const RADIUS_OF_NODE = Math.round(DIAMETER_OF_NODE/2);
-    return(
+export function Heap({ heapArray, width, max_nodes_bottom }) {
+    const MAX_AMOUNT_OF_NODES = max_nodes_bottom;
+    const DIAMETER_OF_NODE = Math.ceil(width / MAX_AMOUNT_OF_NODES);
+    const RADIUS_OF_NODE = Math.ceil(DIAMETER_OF_NODE / 2);
+    return (
         <div className='App'>
             <div style={{
-                width: width+'px',
+                width: width + 10 + 'px',
                 background: 'red',
                 flexWrap: 'wrap',
                 display: 'flex'
-            }}>  
-                {heapArray.map((item,index)=>{Math.log(2)
-                return (
-                    
-                    <div className='node' style={{
-                        marginLeft: calculate_margin(width, index,RADIUS_OF_NODE)-RADIUS_OF_NODE+'px',
-                        marginRight: calculate_margin(width, index,RADIUS_OF_NODE)+'px',
-                        background: 'purple',
-                        display: 'flex',
-                        flexDirection: 'row',
-                        width: RADIUS_OF_NODE+'px',
-                    }}>{item}</div>
-                        )}
+            }}>
+                {heapArray.map((item, index) => {
+                    // Math.log(2)
+                    return (
+
+                        <div className='node' style={{
+                            marginLeft: calculate_margin(width, index, RADIUS_OF_NODE, max_nodes_bottom) + 'px',
+                            marginRight: calculate_margin(width, index, RADIUS_OF_NODE, max_nodes_bottom) + 'px',
+                            marginBottom: '20px',
+                            background: 'purple',
+                            display: 'flex',
+                            flexDirection: 'row',
+                            width: DIAMETER_OF_NODE + 'px',
+                        }}>{item}</div>
                     )
+                }
+                )
                 }
             </div>
         </div>
@@ -39,18 +42,23 @@ export function Heap({heapArray, width}){
 }
 
 //General formula of base conversion
-function dual_logarithm(value){
-    return (Math.log(value)/Math.log(2));
+function dual_logarithm(value) {
+    return (Math.log(value) / Math.log(2));
 }
-function calculate_margin(width, index,radius){
-    console.log("RADIUS "+radius)
+function calculate_margin(width, index, radius, max_nodes_bottom) {
     //Special case for the root
-    if (index==0){
-        return width/2
-    }else{
-        const INDEX_LEVEL = Math.floor(dual_logarithm(index+1));
-        const TOTAL_MARGIN = (width/(Math.pow(2,INDEX_LEVEL)))
-        console.log("Margin: "+TOTAL_MARGIN)
-        return TOTAL_MARGIN/2;
+    //bottom nodes must be squished together in a pixel perfect way.
+    //so we handle this seperately
+    if (index + 2 > max_nodes_bottom) {
+
+        return 0;
+    } else {
+        //we have some rounding issues (due to division, pixel perfect behaviour..)
+        // here so at the bottom stuff starts to get mushy...
+        const INDEX_LEVEL = Math.floor(dual_logarithm(index + 1));
+        const TOTAL_MARGIN = Math.ceil(width / (Math.pow(2, INDEX_LEVEL)))
+        console.log("Margin: " + (TOTAL_MARGIN / 2 - radius))
+        return Math.ceil(TOTAL_MARGIN / 2) - radius;
     }
+
 }
--- src/react-pages/heaps.jsx
+++ src/react-pages/heaps.jsx
@@ -12,40 +12,43 @@ import { eventWrapper } from '@testing-library/user-event/dist/utils/index.js';
 //Add positon tracker // Keys // IDs // Class //Auto resize //Form submit
 
 
-export function Heaps(){
+export function Heaps() {
 
     const [inputBarValue, inputBarValueSet] = useState(0);
     const [arrayOfHeap, arraySet] = useState([]);
     const submitted = function (e) {
         var formValue = e.target.querySelector("#form").value;
         //This merely creates a copy lol..?
-     
-        arraySet([...arrayOfHeap,formValue])
-        console.log("Array "+arrayOfHeap)
+
+        arraySet([...arrayOfHeap, formValue])
+        console.log("Array " + arrayOfHeap)
         e.target.querySelector("#form").value = ""
     }
-    return(
+    return (
         <div className='App'>
             <div id="heap-container">
                 <div id="inner-heap-container">
-                    
-                <Heap  heapArray={arrayOfHeap} width={100}/>
-                <form  onSubmit={(e) => {
-                    submitted(e)}}>
-                    <input onKeyDown={(e)=>{
-                        if (isNaN(e.key/2)&&e.key!="Backspace"&&e.key!="Enter"){
-                            e.preventDefault()
-                        }
-                      
-                      
-                    }} id="form" onChange={(e)=>{}} />
-                    <button type='submit'>Submit</button>
-                </form>
+
+                    <Heap heapArray={arrayOfHeap} width={250} max_nodes_bottom={8} />
+
+                    <form onSubmit={async (e) => {
+                        e.preventDefault()
+                        submitted(e)
+                    }}>
+                        <input onKeyDown={(e) => {
+                            if (isNaN(e.key / 2) && e.key != "Backspace" && e.key != "Enter") {
+                                e.preventDefault()
+                            }
+
+
+                        }} id="form" onChange={(e) => { }} />
+                        <button type='submit'>Submit</button>
+                    </form>
 
                 </div>
             </div>
             <div>
-                
+
             </div>
         </div>
     )
