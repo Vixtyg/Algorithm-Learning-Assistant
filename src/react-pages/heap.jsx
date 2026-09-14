6f39f645d4ff0aa31f1325b61bb272e8c47bb177
Timur Alimov <mygamer1805@gmail.com>
2026-09-14 04:00:34 +0200

    Added a pop effect

--- src/react-pages/heap.jsx
+++ src/react-pages/heap.jsx
@@ -64,6 +64,11 @@ export function Heap({ heapArray, width, max_nodes_bottom, change_tracker, setHe
     {
       // to
       opacity: 1,
+      transform: 'scale(1.2)'
+    }, 
+    {
+      // to
+      opacity: 1,
       transform: 'scale(1)'
     },
   ],
