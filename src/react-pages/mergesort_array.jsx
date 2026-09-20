621105e4a675ef0fd1c192d464fca1a4305df208
Timur Alimov <mygamer1805@gmail.com>
2026-09-20 06:47:44 +0200

    Added ui to sorting

---
+++ src/react-pages/mergesort_array.jsx
@@ -0,0 +1,113 @@
+import '../App.css';
+import LetterGlitch from '../LightPillar';
+import React, { useState, useEffect, useRef } from "react";
+import { createRoot } from 'react-dom/client';
+import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
+import { HashRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { cleanup } from '@testing-library/react';
+import { BST } from './binary_search_tree.jsx';
+import { eventWrapper } from '@testing-library/user-event/dist/utils/index.js';
+import { update } from 'three/examples/jsm/libs/tween.module.js';
+
+
+//Add positon tracker // Keys // IDs // Class //Auto resize //Form submit
+
+
+export function Mergesort({ elements }) {
+    const number = elements
+    const input_bar_array = []
+    const input_bar_ref = useRef([])
+    const submit_button_ref = useRef()
+    const [update_counter, set_update_counter] = useState(0);
+    var i
+    for (i = 0; i < elements; i++) {
+        if (i < (elements - 1)) {
+            input_bar_array.push(',')
+        } else {
+            input_bar_array.push('')
+        }
+    }
+    const go_to_next_input_bar = (e, index) => {
+        if ((index + 2) <= elements) {
+            input_bar_ref.current[index + 1].focus()
+        }
+    }
+    const go_to_previous_input_bar = (e, index) => {
+        if ((index) > 0) {
+            input_bar_ref.current[index - 1].focus()
+        }
+    }
+    const check_if_full = () => {
+        var is_full = false
+        var filled_containers = 0
+        var index
+        for (index = 0; index < elements; index++) {
+            if (input_bar_ref.current[index].value != "") {
+                filled_containers += 1;
+            }
+            if (filled_containers == elements) {
+                unlock_submit()
+            } else {
+                lock_submit()
+            }
+        }
+    }
+    const unlock_submit = () => {
+        submit_button_ref.current.style.opacity = 1
+        submit_button_ref.current.disabled = false
+    }
+    const lock_submit = () => {
+        submit_button_ref.current.style.opacity = 0
+        submit_button_ref.current.disabled = true
+    }
+    const create_animation_matrix = () => {
+
+    }
+    const mergesort = (arr, start, end) => {
+
+    }
+    const merge = (arr1, arr2) => {
+
+    }
+    return (
+        <div className='App'>
+            <div id='array-form'>
+                <div id="input-bars">
+                    [{input_bar_array.map((value, index) => {
+                        return <div id="outer-input-bar-container">
+                            <div id="input-bar-container">
+                                <input onChange={check_if_full} onKeyDown={e => {
+                                    if (e.key == "Enter"
+                                        || e.key == "ArrowRight"
+                                    ) {
+                                        //Note to self - track caret position
+                                        //to improve navigation here!
+                                        go_to_next_input_bar(e, index)
+                                    }
+                                    if (e.key == "ArrowLeft"
+                                    ) {
+                                        //Note to self - track caret position
+                                        //to improve navigation here!
+                                        go_to_previous_input_bar(e, index)
+                                    }
+                                }} id="cell-inputbar" type="text"
+                                    ref={ref => {
+                                        input_bar_ref.current.push(ref)
+                                    }} />
+                                {value}
+                            </div>
+                        </div>
+                    })}]
+
+
+                </div>
+
+                <button disabled="disabled" style={{
+                    opacity: 0
+                }} id="submit-array-form"
+                    onSubmit={create_animation_matrix}
+                    ref={submit_button_ref}>Submit!</button>
+            </div>
+        </div>
+    )
+}
--- src/react-pages/mergesort_quicksort.jsx
+++ src/react-pages/mergesort_quicksort.jsx
@@ -5,7 +5,7 @@ import { createRoot } from 'react-dom/client';
 import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
 import { HashRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import { cleanup } from '@testing-library/react';
-import { Heap } from './heap.jsx';
+import { Mergesort } from './mergesort_array.jsx';
 import { BST } from './binary_search_tree.jsx';
 import { eventWrapper } from '@testing-library/user-event/dist/utils/index.js';
 
@@ -14,134 +14,10 @@ import { eventWrapper } from '@testing-library/user-event/dist/utils/index.js';
 
 
 export function Sorting() {
-    const heapRef = useRef();
-    const [inputBar, setInputBar] = useState();
-    const [inputBarValue, inputBarValueSet] = useState(0);
-    const [arrayOfHeap, arraySet] = useState([]);
-    const [extractMin, setExtractMin] = useState(false);
-    const [heapify, setHeapify] = useState(false);
 
-
-    const BSTRef = useRef();
-    const [inputBarBST, setInputBarBST] = useState();
-    const [inputBarValueBST, inputBarValueSetBST] = useState(0);
-    const [latestBST, setLatestBST] = useState();
-    const [rotate, setRotate] = useState(false);
-
-    const submitted = function (e) {
-        var formValue = e.target.querySelector("#form").value;
-        //This merely creates a copy lol..?
-        if (formValue != "") {
-            arraySet([...arrayOfHeap, formValue])
-        }
-        e.target.querySelector("#form").value = ""
-    }
-    const emptyArray = () => {
-        heapRef.current.style.opacity = 0;
-        setTimeout(() => {
-            arraySet([]);
-
-            heapRef.current.style.opacity = 1;
-        }, 500);
-    }
-    const fixHeap = () => {
-        setHeapify(true)
-
-    }
-    const popHeap = () => {
-        setExtractMin(true)
-
-    }
-    React.useEffect(() => {
-        if (heapRef != null) {
-            if (extractMin == true) {
-                //  heapRef.current.style.background = "red"
-            }
-        }
-    }, [extractMin]);
-
-    const submittedBST = function (e) {
-        var formValue = e.target.querySelector("#form-BST").value;
-        //This merely creates a copy lol..?
-        if (formValue != "") {
-            setLatestBST(formValue)
-        }
-        e.target.querySelector("#form-BST").value = ""
-    }
-    const emptyArrayBST = () => {
-        BSTRef.current.style.opacity = 0;
-        setTimeout(() => {
-            setLatestBST("RESET");
-        }, 500);
-        setTimeout(() => {
-            BSTRef.current.style.opacity = 1;
-        }, 600);
-    }
     return (
         <div className='App'>
-            <div id="heap-container">
-                <div id="inner-heap-container">
-                    <div id="heap-wrapper" ref={heapRef}>
-                        <Heap id="heap" heapArray={arrayOfHeap} width={Math.round(// Source - https://stackoverflow.com/a/3437825
-                            window.screen.width / 4)} max_nodes_bottom={8} setHeapArray={arraySet}
-                            input_bar={inputBar}
-                            extract_min={extractMin} set_extract_min={setExtractMin} heapifyStat={heapify} setHeapify={setHeapify} />
-
-                    </div>
-
-                    <form autoComplete='off' onSubmit={async (e) => {
-                        e.preventDefault()
-                        submitted(e)
-                    }}>
-                        <input ref={ref => {
-                            setInputBar(ref)
-                        }} onKeyDown={(e) => {
-                            if (isNaN(e.key / 2) && e.key != "Backspace" && e.key != "Enter"
-                                || arrayOfHeap.length >= 15) {
-                                e.preventDefault()
-                            }
-
-
-                        }} id="form" onChange={(e) => { }} />
-                        <button type='submit'>Submit</button>
-                    </form>
-                    <button onClick={emptyArray}>Reset</button>
-                    <button onClick={fixHeap}>Heapify</button>
-                    <button onClick={popHeap}>Extract-Min</button>
-                </div>
-            </div>
-            <div id="BST-container">
-                <div id="BST-inner-container">
-                    <div ref={BSTRef} >
-                        <BST id="BST" new_entry_bst={latestBST} set_new_entry_bst={setLatestBST}
-                            width={Math.round(// Source - https://stackoverflow.com/a/3437825
-                                window.screen.width / 4)} max_nodes_bottom={8} />
-                    </div>
-                    <form id="form-BST" autoComplete='off' onSubmit={async (e) => {
-                        e.preventDefault()
-                        submittedBST(e)
-                    }}>
-                        <input ref={ref => {
-                            setInputBarBST(ref)
-                        }} onKeyDown={(e) => {
-                            if (isNaN(e.key / 2) && e.key != "Backspace" && e.key != "Enter"
-                                || arrayOfHeap.length >= 15) {
-                                e.preventDefault()
-                            }
-
-
-                        }} id="form-BST" onChange={(e) => { }} />
-                        <button type='submit'>Submit</button>
-                    </form>
-                    <div>
-
-                        <button onClick={emptyArrayBST}>Reset</button>
-                        <button onClick={fixHeap}>Heapify</button>
-                        <button onClick={popHeap}>Extract-Min</button>
-                    </div>
-
-                </div>
-            </div>
+            <Mergesort elements={10} />
         </div>
     )
 }
